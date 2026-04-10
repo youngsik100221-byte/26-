1075db7d5f07f8a6c79118657dffd53b35be6685
youngsik100221-byte <youngsik100221@gmail.com>
2026-04-10 15:35:57 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
 import streamlit as at
 
 st.title('나의 첫 웹서비스 만들기!!!')
+st.text_input('이름을 입력하세요 : ')
+st.selectbox('좋아하는 음식을 선택하세요 : ', ['떡볶이', '치즈볼'])
+
+if st.button('인삿말 생성') :
+  st.write(f'{name}님! 당신이 좋아하는 음식은 {menu}군요')
+  st.snow()
